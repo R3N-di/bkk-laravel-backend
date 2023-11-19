d1dcde4f466415d86bcad5a169b1d89743fa0194
R3N-di <rendiramadhanmars@gmail.com>
2023-11-19 14:43:01 +0700

    buat controller lamaran dan model nya dan progress api

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::post('/login', [AuthController::class,'login']);
+// Route::post('/login', [AuthController::class,'login']);
+
+require __DIR__.'/lowongan.php';
+// require __DIR__.'/kategori_prestasi.php';
